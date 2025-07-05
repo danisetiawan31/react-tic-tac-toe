61888f08efdb830223464ced175655d661b98da7
Ahmad Dhani Setiawan <dnistwn31@gmail.com>
2025-07-06 06:11:29 +0700

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-tictactoe/',
+  base: '/react-tic-tac-toe/',
   plugins: [react()],
 })
